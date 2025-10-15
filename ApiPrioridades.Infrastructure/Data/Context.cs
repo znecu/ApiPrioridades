d2ab2f28b30ed15c4ce693b8647e2bd23d84326c
Alma Maria <alma_maria@ucne.edu.do>
2025-10-14 23:01:40 -0400

    Se creo Context.cs

---
+++ ApiPrioridades.Infrastructure/Data/Context.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApiPrioridades.Core.Entities;
+
+namespace ApiPrioridades.Infrastructure.Data
+{
+    internal class Context : DbContext
+    {
+        public Context(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Prioridades> Prioridades { get; set; }
+    }
+}
