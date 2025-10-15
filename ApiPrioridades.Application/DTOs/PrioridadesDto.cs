151f3f90ffef5dc891ffddd06c708f409bbab308
Alma Maria <alma_maria@ucne.edu.do>
2025-10-14 23:06:21 -0400

    Se creo PrioridadesDto

---
+++ ApiPrioridades.Application/DTOs/PrioridadesDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiPrioridades.Application.DTOs
+{
+    public class PrioridadesDto
+    {
+        public string Descripcion { get; set; } = string.Empty;
+    }
+}
