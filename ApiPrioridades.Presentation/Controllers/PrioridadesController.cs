15a50af43838c182309ce373569ddf2d45b34b53
Alma Maria <alma_maria@ucne.edu.do>
2025-10-15 13:23:05 -0400

    controller

---
+++ ApiPrioridades.Presentation/Controllers/PrioridadesController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ApiPrioridades.Core.Entities;
+using ApiPrioridades.Infrastructure.Data;
+
+namespace ApiPrioridades.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PrioridadesController : ControllerBase
+    {
+        private readonly Context _context;
+
+        public PrioridadesController(Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Prioridades
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Prioridades>>> GetPrioridades()
+        {
+            return await _context.Prioridades.ToListAsync();
+        }
+
+        // GET: api/Prioridades/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Prioridades>> GetPrioridades(int id)
+        {
+            var prioridades = await _context.Prioridades.FindAsync(id);
+
+            if (prioridades == null)
+            {
+                return NotFound();
+            }
+
+            return prioridades;
+        }
+
+        // PUT: api/Prioridades/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPrioridades(int id, Prioridades prioridades)
+        {
+            if (id != prioridades.PrioridadesId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(prioridades).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PrioridadesExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Prioridades
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Prioridades>> PostPrioridades(Prioridades prioridades)
+        {
+            _context.Prioridades.Add(prioridades);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPrioridades", new { id = prioridades.PrioridadesId }, prioridades);
+        }
+
+        // DELETE: api/Prioridades/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePrioridades(int id)
+        {
+            var prioridades = await _context.Prioridades.FindAsync(id);
+            if (prioridades == null)
+            {
+                return NotFound();
+            }
+
+            _context.Prioridades.Remove(prioridades);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PrioridadesExists(int id)
+        {
+            return _context.Prioridades.Any(e => e.PrioridadesId == id);
+        }
+    }
+}
