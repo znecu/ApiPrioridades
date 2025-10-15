e9e4b87911cf67f7c7f28369c32ca4930e80a476
Alma Maria <alma_maria@ucne.edu.do>
2025-10-14 23:00:26 -0400

    Se creo entidad modelo Prioridades

---
+++ ApiPrioridades.Core/Entities/Prioridades.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiPrioridades.Core.Entities
+{
+    public class Prioridades
+    {
+        [Key]
+        public int PrioridadesId { get; set; }
+
+        [Required(ErrorMessage ="La Descripcion es requerida.")]
+        public string Descripcion { get; set; } = null!;
+    }
+}
